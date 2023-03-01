d86a89756f243a3c43627ffab2963592fa9cca12
kucheroff <kucheroff@tut.by>
2023-03-01 13:31:26 +0300

    project_1 ready

--- Seminar_1/Project_1/Program.cs
+++ Seminar_1/Project_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Первое число: ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.Write("Второе число: ");
+int num_2 = int.Parse(Console.ReadLine());
+if (num_2 * num_2 == num_1)
+{
+    Console.Write($"Число {num_1} является квадратом числа {num_2}.");
+}
+else
+{
+    Console.Write($"Число {num_1} НЕ является квадратом числа {num_2}.");    
+}
