81efd7732901cb6bda9f6e285d07d3f2556c6ecf
kucheroff <kucheroff@tut.by>
2023-03-01 13:49:22 +0300

    project_2 ready

---
+++ Seminar_1/Project_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Номер дня недели: ");
+int num_1 = int.Parse(Console.ReadLine());
+if (num_1 == 1) {Console.Write("Понедельник");};
+if (num_1 == 2) {Console.Write("Вторник");};
+if (num_1 == 3) {Console.Write("Среда");};
+if (num_1 == 4) {Console.Write("Четверг");};
+if (num_1 == 5) {Console.Write("Пятница");};
+if (num_1 == 6) {Console.Write("Суббота");};
+if (num_1 == 7) {Console.Write("Воскресенье");};
